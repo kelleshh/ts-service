84e1e2cddb7d14472ca61a37135e50cdf8516650
kelleshh <shabieieie@gmail.com>
2026-02-23 22:04:33 +0300

    feat(dependencies): добавлены провайдеры для /auto_fit

--- src/tsfit/bootstrap/di.py
+++ src/tsfit/bootstrap/di.py
@@ -6,13 +6,18 @@ from tsfit.application.ports import (
     DatasetParser,
     TimeSeriesDatasetBuilder,
     ModelTrainer,
+    HyperparameterTuner,
 )
-from tsfit.application.usecases import TrainModelUseCase, GetRunUseCase
+
+from tsfit.application.usecases.fit_usecase import TrainModelUseCase
+from tsfit.application.usecases.autofit_usecase import TrainModelAutoUseCase
+from tsfit.application.usecases.get_usecase import GetRunUseCase
 
 from tsfit.infrastructure.store.repository_inmemory import InMemoryTrainingRunRepository
 from tsfit.infrastructure.dataset.builder_polars import PolarsTimeSeriesDatasetBuilder
 from tsfit.infrastructure.dataset.parser_polars import PolarsDatasetParser
 from tsfit.infrastructure.training.trainer_xgb import XGBModelTrainer
+from tsfit.infrastructure.training.tuning_optuna import OptunaXGBTuner
 
 
 class AppProvider(Provider):
@@ -30,7 +35,13 @@ class AppProvider(Provider):
 
     @provide(scope=Scope.APP)
     def trainer(self) -> ModelTrainer:
+        # обучатель без optuna
         return XGBModelTrainer()
+
+    @provide(scope=Scope.APP)
+    def tuner(self) -> HyperparameterTuner:
+        # автоподбор (optuna) инфраструктурный сервис
+        return OptunaXGBTuner()
 
     @provide(scope=Scope.REQUEST)
     def train_uc(
@@ -41,6 +52,19 @@ class AppProvider(Provider):
         trainer: ModelTrainer,
     ) -> TrainModelUseCase:
         return TrainModelUseCase(repo=repo, parser=parser, builder=builder, trainer=trainer)
+
+    @provide(scope=Scope.REQUEST)
+    def train_auto_uc(
+        self,
+        repo: TrainingRunRepository,
+        parser: DatasetParser,
+        builder: TimeSeriesDatasetBuilder,
+        trainer: ModelTrainer,
+        tuner: HyperparameterTuner,
+    ) -> TrainModelAutoUseCase:
+        return TrainModelAutoUseCase(
+            repo=repo, parser=parser, builder=builder, trainer=trainer, tuner=tuner
+        )
 
     @provide(scope=Scope.REQUEST)
     def get_run_uc(self, repo: TrainingRunRepository) -> GetRunUseCase:
