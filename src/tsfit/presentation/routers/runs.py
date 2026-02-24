89701566a8bd1b382091c05eb138f27e122f19b8
kelleshh <shabieieie@gmail.com>
2026-02-24 16:05:25 +0300

    refactor(status_code): change 404 -> status.HTTP_404_NOT_FOUND

--- src/tsfit/presentation/routers/runs.py
+++ src/tsfit/presentation/routers/runs.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, status as s
 
 from dishka.integrations.fastapi import DishkaRoute, FromDishka
 
@@ -15,7 +15,7 @@ router = APIRouter(prefix='/runs', route_class=DishkaRoute, tags=['runs'])
 async def get_run(run_id: str, uc: FromDishka[GetRunUseCase]) -> RunInfoResponse:
     run = uc.execute(run_id)
     if not run:
-        raise HTTPException(status_code=404, detail='run не найден')
+        raise HTTPException(status_code=s.HTTP_404_NOT_FOUND, detail='run не найден')
 
     return RunInfoResponse(
         run_id=run.run_id,
@@ -30,6 +30,6 @@ async def get_run(run_id: str, uc: FromDishka[GetRunUseCase]) -> RunInfoResponse
 async def get_run_result(run_id: str, uc: FromDishka[GetRunUseCase]) -> RunResultResponse:
     run = uc.execute(run_id)
     if not run:
-        raise HTTPException(status_code=404, detail='run не найден')
+        raise HTTPException(status_code=s.HTTP_404_NOT_FOUND, detail='run не найден')
 
     return RunResultResponse(run_id=run.run_id, result=run.result)
