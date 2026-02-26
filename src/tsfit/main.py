d0f09bb1e8581bda32edd59563f56ce2441f6d1d
kelleshh <shabieieie@gmail.com>
2026-02-26 15:50:35 +0300

    presentation(app): обновлена точка входа и подключение новых роутеров

--- src/tsfit/main.py
+++ src/tsfit/main.py
@@ -1,28 +1,32 @@
+from __future__ import annotations
+
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from dishka.integrations.fastapi import setup_dishka
 
-from tsfit.presentation.routes import router as tsfit_router
+from tsfit.presentation.routers import router as tsfit_router
 from tsfit.bootstrap.di import container
 
-from contextlib import asynccontextmanager
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     '''
-    Жизненный цикл приложения. В конце цикла закрываем контейнер dishka
+    Жизненный цикл приложения. D конце цикла закрываем контейнер dishka
     '''
     yield
     await app.state.dishka_container.close()
 
+
 def create_app() -> FastAPI:
     '''
-    Создание и настройка FastAPI-приложения
+    Cоздание и настройка fastapi-приложения
     '''
     app = FastAPI(
         title='XGBoost Time Series train API',
-        description='Сервис для обучения XGBoost на временных рядах',
-        version='0.1.3',
-        lifespan=lifespan
+        description='Cервис для обучения xgboost на временных рядах',
+        version='0.2.0',
+        lifespan=lifespan,
     )
     app.include_router(tsfit_router)
 
